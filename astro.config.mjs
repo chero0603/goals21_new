81d214df1277f15035b15916f8f01dfcabea0c82
Matsu <chero@grooow.com>
2025-08-04 01:59:43 +0900

    アイコンの依存関係を追加し、Coverageコンポーネントに国とアイコンのマッピングを実装。Difficulties、Problems、ServiceDetailsコンポーネントのテキストを改良し、レスポンシブデザインを強化。

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,6 +11,9 @@ export default defineConfig({
       include: {
         mdi: ["*"],
         "mdi-light": ["*"],
+        "flag": ["*"],
+        "emojione": ["*"],
+        "twemoji": ["*"],
       },
     }),
   ],
